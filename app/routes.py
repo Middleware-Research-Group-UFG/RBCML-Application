0f3ea4ef684ce6e63ccbdc2618ae041012c968a5
luis-fbs <luis-fbs@outlook.com>
2025-05-02 12:30:43 -0300

    Endpoints

---
+++ app/routes.py
@@ -0,0 +1,51 @@
+from flask import Blueprint, render_template, redirect, request
+
+from .RBCMLModel import RBCMLModel
+from .events import get_user_role
+
+main = Blueprint('main', __name__)
+
+
+@main.route('/ping')
+def view_ping():
+    return 'pong'
+
+@main.route('/')
+def view_index():
+    return redirect('/login')
+
+@main.route('/login', methods=['GET', 'POST'])
+def view_login():
+    if request.method == 'GET':
+        return render_template('login.html', roles=RBCMLModel.get_role_names())
+    else:
+        role = request.form.get('option')
+        return redirect(f'/user/{role}')
+
+@main.route('/user/<user>')
+def view_user(user):
+    return render_template('user.html', user=user)
+
+@main.route('/user/<user>/session/<session>')
+def view_session(user, session):
+    role = get_user_role(user, session)
+    return render_template('session.html', user=user, session=session, role=role)
+
+@main.route('/createRole', methods=['GET', 'POST'])
+def view_create_role():
+    if request.method == 'GET':
+        return render_template('createRole.html')
+    else:
+        role = request.form
+        sV = 'sendVideo' in role
+        rV = 'receiveVideo' in role
+        sA = 'sendAudio' in role
+        rA = 'receiveAudio' in role
+        sS = 'sendString' in role
+        rS = 'receiveString' in role
+        capability = (sV, rV, sA, rA, sS, rS)
+
+        if RBCMLModel.set_role(role.get('roleName'), capability):
+           return "Role created successfully"
+        else:
+           return "Role not created"
