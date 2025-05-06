6025ae8a745c1337502b68506508225c6f1ce7b7
luis-fbs <luis-fbs@outlook.com>
2025-05-06 15:53:19 -0300

    Search for User, Model or Session

--- app/database/db.py
+++ app/database/db.py
@@ -11,5 +11,16 @@ def insert(data, table, db="rbcml.db"):
             cursor.execute(query)
             return (201, "Created")
         except:
-            return (400, "Bad request")
+            return (400, "Bad request!")
+
+def search(data, key, table, db="rbcml.db"):
+    with sqlite3.connect(db) as connection:
+        cursor = connection.cursor()
+        query = f"""SELECT * FROM {table}
+                        WHERE {key} = '{data}'"""
+        try:
+            cursor.execute(query)
+            return cursor.fetchall()
+        except:
+            return []
 
