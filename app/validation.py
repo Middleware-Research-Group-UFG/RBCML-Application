e5369d01fabaa0b5a6792a534e2e171517793406
luis-fbs <luis-fbs@outlook.com>
2025-05-08 17:10:23 -0300

    refactor functions and add extra validation
    rename function validate_json to validate_model to better fit the application context,
    create auxiliary functions to keep the code clean.

--- app/validation.py
+++ app/validation.py
@@ -4,38 +4,45 @@ import json
 
 def validate_string(input_str):
     pattern = re.compile(r'^[a-zA-Z0-9 _.@]+$')
-    if not pattern.match(input_str):
-        raise ValueError("Invalid string. Only alphanumeric characters, spaces, '_', '.', and '@' are allowed.")
+    return pattern.match(input_str)
+
+def keys_are_sorted(keys):
+    return all(keys[i] <= keys[i+1] for i in range(len(keys) - 1))
+
+def model_connection_is_valid(roles, key, value):
+    if not isinstance(value, list):
+        return False
+
+    key_roles = key.split("-")
+    if len(value) != len(key_roles):
+        return False
+    if not all(role in roles for role in key_roles):
+        return False
+    if not keys_are_sorted(key_roles):
+        return False
+    
+    for capabilities in value:
+        if not isinstance(capabilities, list) or len(capabilities) != 8 or not all(isinstance(cap, bool) for cap in capabilities):
+            return False
+    
     return True
 
-def validate_json(input_json):
-    
-    if "roles" not in input_json or "connections" not in input_json:
+
+def validate_model(input_model):    
+    if len(input_model) != 2 or "roles" not in input_model or "connections" not in input_model:
         return False
-    if not isinstance(input_json["roles"], list) or not isinstance(input_json["connections"], dict):
+    if not isinstance(input_model["roles"], list) or not isinstance(input_model["connections"], dict):
         return False
 
-    roles = input_json["roles"]
-    connections = input_json["connections"]
+    roles = sorted(input_model["roles"])
+    if not all(validate_string(role) for role in roles):
+        return False
 
-    
+    connections = input_model["connections"]
+    if "-".join(roles) not in connections:
+        return False
     for connection_key, connection_value in connections.items():
-        key_roles = connection_key.split("-")
-
-        for role in key_roles:
-            if role not in roles:
-                return False  
-
-        if not isinstance(connection_value, list):
-            return False  
-        if len(connection_value) != len(key_roles):
-            return False  
-
-        for sublist in connection_value:
-            if not isinstance(sublist, list) or len(sublist) != 8:
-                return False  
-            for item in sublist:
-                if not isinstance(item, bool):
-                    return False  
+        if not model_connection_is_valid(roles, connection_key, connection_value):
+            return False
 
     return True  
