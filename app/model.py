bf4c9fae0b8d441b121a565298cf8168bbcef003
luis-fbs <luis-fbs@outlook.com>
2025-05-09 23:40:30 -0300

    Model class

---
+++ app/model.py
@@ -0,0 +1,7 @@
+class Model():
+    def __init__(self, model_id, name, description, definition):  
+        self.model_id = model_id
+        self.name = name
+        self.description = description
+        self.definition = definition
+
