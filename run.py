8efba604dfe005a6695ffa45d63a187c160e691e
luis-fbs <luis-fbs@outlook.com>
2025-05-09 23:00:16 -0300

    Change SSL path

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from app import create_app, socketio
 
 app = create_app()
 
-socketio.run(app, host='0.0.0.0', port=5000, debug=True, ssl_context=('cert.pem', 'key.pem'))
+socketio.run(app, host='0.0.0.0', port=5000, debug=True, ssl_context=('.ssl/cert.pem', '.ssl/key.pem'))
