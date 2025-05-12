4d379dd8dc13f714607d32f4bf905a8829587795
luis-fbs <luis-fbs@outlook.com>
2025-05-11 23:18:19 -0300

    Create functions to send invite for each participant in the session and loads participants from json to better fit the database

--- app/session.py
+++ app/session.py
@@ -1,3 +1,8 @@
+from .database.db import search
+from .email import send
+from json import loads
+
+
 class Session:
     def __init__(self, session_id, creator, model_id, creation_date, start_date, expiration_date, participants):
         self.session_id = session_id
@@ -6,5 +11,22 @@ class Session:
         self.creation_date = creation_date
         self.start_date = start_date
         self.expiration_date = expiration_date
-        self.participants = participants
+        self.participants = loads(participants)
+
+    def invite_participants(self, url_ip, url_port):
+        for user in self.participants:
+            email = search(user, "Tag", "User")[0][2]
+            subject = f"Invitation" 
+            content = f"""
+                {self.creator} invited you to participate in session {self.session_id}
+                as {self.participants[user]}
+        
+                You can access the session using the link:
+                
+                https://{url_ip}:{url_port}/invite?session={self.session_id}&user={user}
+                
+                Valid from: {self.start_date}
+                      to: {self.expiration_date}"""
+
+            send(subject, content, email)
 
