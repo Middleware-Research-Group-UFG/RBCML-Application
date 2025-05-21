2a13551ff122fcc305571ca002797f6948247e0e
luis-fbs <luis-fbs@outlook.com>
2025-05-20 21:54:37 -0300

    Change name to avoid colision with token module

---
+++ app/token_handler.py
@@ -0,0 +1,18 @@
+import jwt
+from os import getenv
+from pathlib import Path
+
+path = Path(__file__).parent
+
+def create(payload):
+    with open(path/"keys/private_key.pem", "r") as file:
+        private_key = file.read()
+        return jwt.encode(payload, private_key, "RS256")
+
+def decode(token, options):
+    with open(path/"keys/public_key.pem", "r") as file:
+        public_key = file.read()
+        try:
+            return jwt.decode(token, public_key, "RS256", options)
+        except:
+            return {}
