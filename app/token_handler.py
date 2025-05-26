626f8129bb38f9b621c1209cd39da349151f2808
luis-fbs <luis-fbs@outlook.com>
2025-05-26 18:19:35 -0300

    Function to genereta decode options

--- app/token_handler.py
+++ app/token_handler.py
@@ -16,3 +16,13 @@ def decode(token, options):
             return jwt.decode(token, public_key, "RS256", options)
         except:
             return {}
+
+def generate_default_decode_options(required):
+    default = {
+        "verify_signature": True,
+        "verify_iat": "verify_signature",
+        "verify_nbf": "verify_signature",
+        "verify_exp": "verify_signature",
+        "require": ["iat", "nbf", "exp"] + required
+    }
+    return default
